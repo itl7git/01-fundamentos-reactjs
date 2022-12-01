9bd1b37a795cfb2394ffafdc5f83e4a34a33fb1b
Italo Leal <itlcoorporativo@gmail.com>
2022-11-30 22:09:09 -0300

    Propriedades: passando parametros para o componente Post para criação de varios conseguindo reaproveitar codigo

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ export function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Italo"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, eveniet aperiam consectetur animi ducimus sint officia et corporis nisi quam quisquam officiis. Consectetur, est officiis reiciendis repellendus enim natus quam!"
+      />
+      <Post 
+        author="Ailton"
+        content="Fale brow, sit amet consectetur adipisicing elit. Sit, eveniet aperiam consectetur animi ducimus sint officia et corporis nisi quam quisquam officiis. Consectetur, est officiis reiciendis repellendus enim natus quam!"
+      />      
     </div>
   )
 }
