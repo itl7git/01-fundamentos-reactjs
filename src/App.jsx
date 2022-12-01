1e6984d947dee92477f83ad6783dfa72dab6e821
Italo Leal <itlcoorporativo@gmail.com>
2022-11-30 21:52:42 -0300

    Criação de Components e forma de declaração com export

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { Post } from './Post';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
   return (
-    <h1>hELLO WORLD!</h1>
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
   )
 }
-
-export default App
