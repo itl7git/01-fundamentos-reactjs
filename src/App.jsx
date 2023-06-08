d5cafb59b330b3bac832d744a4734055b0a392b6
Italo Leal <itlcoorporativo@gmail.com>
2023-06-08 19:12:10 -0300

    Sidebar: Criando o componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <div>
       <Header />
-
-      <Post
-        author="Italo"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, eveniet aperiam consectetur animi ducimus sint officia et corporis nisi quam quisquam officiis. Consectetur, est officiis reiciendis repellendus enim natus quam!"
-      />
-      <Post
-        author="Ailton"
-        content="Fale brow, sit amet consectetur adipisicing elit. Sit, eveniet aperiam consectetur animi ducimus sint officia et corporis nisi quam quisquam officiis. Consectetur, est officiis reiciendis repellendus enim natus quam!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Italo"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, eveniet aperiam consectetur animi ducimus sint officia et corporis nisi quam quisquam officiis. Consectetur, est officiis reiciendis repellendus enim natus quam!"
+          />
+          <Post
+            author="Ailton"
+            content="Fale brow, sit amet consectetur adipisicing elit. Sit, eveniet aperiam consectetur animi ducimus sint officia et corporis nisi quam quisquam officiis. Consectetur, est officiis reiciendis repellendus enim natus quam!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1686164194855-e4d37f4e7c2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <strong>Italo Leal</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
