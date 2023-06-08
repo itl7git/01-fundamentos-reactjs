bd1cca2d1c645bdbdfb11fa3579571de6b965132
Italo Leal <itlcoorporativo@gmail.com>
2023-06-08 19:39:31 -0300

    Sidebar: Finalizando o componente. Alinhando o strong, span; Adicionando Icone; Adicionando hover no icone;

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,17 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=826&t=st=1686262408~exp=1686263008~hmac=f992b422dfac22191f0bbba95f5a1f13cd4112b0dcfd8f85f3dc4320629fa1fe" />
+
         <strong>Italo Leal</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
