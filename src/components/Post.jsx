e337c56c1fdc17cec3e81643f64f29325bbd7d43
Italo Leal <itlcoorporativo@gmail.com>
2023-06-09 13:20:53 -0300

    Post: Criando a estrutura Html do Header e Content

---
+++ src/components/Post.jsx
@@ -0,0 +1,32 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://images.unsplash.com/photo-1686164194855-e4d37f4e7c2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Italo Leal</strong>
+            <span>Web Developer</span>
+          </div>
+
+          <time title="11 de maio ás 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        </div>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+
+      </div>
+
+    </article>
+  );
+}
